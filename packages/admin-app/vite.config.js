c1784b8acb539b42b59e8be0959d2f3b35265d9e
Michael Sean Becker <michael@ritual.com>
2025-10-08 22:14:40 -0700

    chore(admin): Fix page load

--- packages/admin-app/vite.config.js
+++ packages/admin-app/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.',
   publicDir: 'public',
   build: {
     target: 'esnext',
     outDir: 'dist',
-    rollupOptions: {
-      input: resolve(__dirname, 'public/index.html'),
-    },
   },
   optimizeDeps: {
     include: ['@preact/signals-core', 'lit', '@supabase/supabase-js', '@shoelace-style/shoelace'],
